547f675ce36e19733de0bd9980ad1ae7a45102ee
sk-226 <skurita0208@gmail.com>
2024-11-28 11:38:22 +0900

    Vector関連の関数ok

--- src/vector.c
+++ src/vector.c
@@ -1,28 +1,106 @@
 /* src/vector.c */
 #include "vector.h"
 
-// ベクトルの作成
+/* ベクトルの作成 */
 Vector *create_vector(int size) {
   Vector *vec = (Vector *)malloc(sizeof(Vector));
 
   vec->size = size;
-  vec->data = (double *)calloc(size, sizeof(double));
+  vec->values = (double *)calloc(size, sizeof(double));
 
   return vec;
 }
 
-// ベクトルの解放
+/* ベクトルの解放 */
 void free_vector(Vector *vec) {
   if (vec != NULL) {
-    free(vec->data);
+    free(vec->values);
     free(vec);
   }
 }
 
-// ベクトルの表示
+/* ベクトルの表示 */
 void print_vector(const Vector *vector) {
   printf("Vector of size: %d\n", vector->size);
   for (int i = 0; i < vector->size; i++) {
-    printf(" [%d] = %lf\n", i, vector->data[i]);
+    printf(" [%d] = %e\n", i, vector->values[i]);
+  }
+}
+
+/* ベクトル同士の積 */
+double vec_dot(const Vector *vec1, const Vector *vec2) {
+  if (vec1->size != vec2->size) {
+    fprintf(stderr,
+            "Error: Vector sizes do not match in file %s at line # %d \n",
+            __FILE__, __LINE__);
+    exit(EXIT_FAILURE);
+  }
+
+  double result = 0.0;
+
+  for (int i = 0; i < vec1->size; i++) {
+    result += vec1->values[i] * vec2->values[i];
+  }
+
+  return result;
+}
+
+/* ベクトルのスカラー倍 */
+void vec_scale(Vector *vec, double scalar) {
+  for (int i = 0; i < vec->size; i++) {
+    vec->values[i] *= scalar;
+  }
+}
+
+/* ベクトルの加算 */
+void vec_add(Vector *vec1, const Vector *vec2, Vector *result) {
+  if (vec1->size != vec2->size || vec1->size != result->size) {
+    fprintf(stderr,
+            "Error: Vector sizes do not match in file %s at line # %d \n",
+            __FILE__, __LINE__);
+    exit(EXIT_FAILURE);
+  }
+
+  for (int i = 0; i < vec1->size; i++) {
+    result->values[i] = vec1->values[i] + vec2->values[i];
+  }
+}
+
+/* ベクトルの減算 */
+void vec_sub(Vector *vec1, const Vector *vec2, Vector *result) {
+  if (vec1->size != vec2->size || vec1->size != result->size) {
+    fprintf(stderr,
+            "Error: Vector sizes do not match in file %s at line # %d \n",
+            __FILE__, __LINE__);
+    exit(EXIT_FAILURE);
+  }
+
+  for (int i = 0; i < vec1->size; i++) {
+    result->values[i] = vec1->values[i] - vec2->values[i];
+  }
+}
+
+/* ベクトルの2-ノルムを計算 */
+double vec_norm(const Vector *vec) {
+  double result = 0.0;
+
+  for (int i = 0; i < vec->size; i++) {
+    result += vec->values[i] * vec->values[i];
+  }
+
+  return sqrt(result);
+}
+
+/* ベクトルをコピー */
+void vec_copy(const Vector *src, Vector *dest) {
+  if (src->size != dest->size) {
+    fprintf(stderr,
+            "Error: Vector sizes do not match in file %s at line # %d \n",
+            __FILE__, __LINE__);
+    exit(EXIT_FAILURE);
+  }
+
+  for (int i = 0; i < src->size; i++) {
+    dest->values[i] = src->values[i];
   }
 }

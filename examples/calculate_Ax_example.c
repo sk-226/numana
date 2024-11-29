e9dc92983b83c445d53885eaa482ffa5a8e3ec14
sk-226 <skurita0208@gmail.com>
2024-11-29 19:35:39 +0900

    add example/test codes

---
+++ examples/calculate_Ax_example.c
@@ -0,0 +1,31 @@
+#include "csc_matrix.h"
+#include "vector.h"
+
+int main(void) {
+  const char *filepath = "./data/LFAT5.rb";
+  CSCMatrix *matrix = read_rb_matrix(filepath);
+  if (matrix == NULL) {
+    fprintf(stderr, "Failed to load matrix from file: %s\n", filepath);
+    return 1;
+  }
+  print_csc_matrix(matrix);
+
+  Vector *x = create_vector(matrix->num_cols);
+  for (int i = 0; i < x->size; i++) {
+    x->values[i] = 1.0;
+  }
+  Vector *result = create_vector(matrix->num_rows);
+
+  print_vector(x);
+
+  mat_vec_mul(matrix, x, result);
+
+  print_vector(result);
+
+  // メモリの解放
+  free_csc_matrix(matrix);
+  free_vector(x);
+  free_vector(result);
+
+  return 0;
+}
---
+++ examples/print_csr_matrix_example.c
@@ -0,0 +1,18 @@
+#include "csr_matrix.h"
+
+int main(void) {
+  const char *filepath = "./data/LFAT5.rb";
+  CSRMatrix *matrix = read_rb_matrix_to_csr(filepath);
+  if (matrix == NULL) {
+    fprintf(stderr, "Failed to load matrix from file: %s\n", filepath);
+    return 1;
+  }
+
+  // 必要に応じて行列の内容を表示
+  print_csr_matrix(matrix);
+
+  // メモリの解放
+  free_csr_matrix(matrix);
+
+  return 0;
+}
